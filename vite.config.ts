72a8f9d6555484c7b9d6b07ea62c40e3b279f1dc
☘ <kogawananari@gmail.com>
2025-08-05 17:37:02 +0800

    :art: 調整封面,打包添加dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { resolve } from "node:path"
 import { defineConfig } from "vite"
 import alias from "@holy-two/vite-plugin-alias"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
-    plugins: [alias()],
+    plugins: [
+        alias(),
+        dts({
+            include: ["src/lib"],
+            rollupTypes: true,
+            compilerOptions: {
+                types: ["./.astro/types.d.ts"],
+            },
+        }),
+    ],
     build: {
         lib: {
             entry: resolve("./src/lib/index.ts"),
